eab3d2ab547f8a5acb3cb5ca06b48b2869ca5b70
Mike Brown <mike@mbrown.co>
2026-08-27 14:49:04 +0000

    Add Meta Pixel (805390589299003) - based on last working commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './styles.css';
+import Script from 'next/script';
 
 export const metadata = {
   title: 'The Freedom Audit',
@@ -13,6 +14,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '805390589299003');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=805390589299003&ev=PageView&noscript=1"
+          />
+        </noscript>
         {children}
         <script dangerouslySetInnerHTML={{
           __html: `
